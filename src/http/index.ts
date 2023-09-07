76ae171646cf71c45c85688035d7f84ee90c6148
eullerg <euller_germano1@hotmail.com>
2023-09-07 02:58:34 -0300

    updated host

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const httpClient: AxiosInstance = axios.create({
-  baseURL: "https://register-api-chi.vercel.app/",
+  baseURL: "https://register-api-rosy.vercel.app/",
   headers: {
     "Content-type": "application/json",
   },
