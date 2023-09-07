43c641bfd2b5120e19c88a327ccf47e3eb528d36
eullerg <euller_germano1@hotmail.com>
2023-09-07 15:05:55 -0300

    updated URL

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const httpClient: AxiosInstance = axios.create({
-  baseURL: "https://your-way-tracker.vercel.app/",
+  baseURL: "https://your-way-tracker.vercel.app/#/",
   headers: {
     "Content-type": "application/json",
   },
